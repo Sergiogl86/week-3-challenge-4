79cc01f11103c1ce70f0ed795ffea03afe444ced
FranMedic <m.francisca.medico@gmail.com>
2021-10-14 20:07:58 +0200

    added babel plugin for runing test in jest, modified failed test

--- public/app/test/Page.test.js
+++ public/app/test/Page.test.js
@@ -3,13 +3,14 @@ import Page from "../js/Page.js";
 describe("When given a Page class", () => {
   describe("when it recieves a parentElement `section`, an html tag `div`and a className `firstson`", () => {
     test("then it should render a div element with class`firstson`, inside de section container", () => {
-      const parentElement = document.createElement("section");
+      const container = document.createElement("body");
+
       const tag = "div";
       const className = "firstson";
 
-      const elementToCreate = new Page(parentElement, className, tag);
+      const elementToCreate = new Page(container, tag);
 
-      const elementCreated = parentElement.querySelector("div.firstson");
+      const elementCreated = container.querySelector("div");
 
       expect(elementCreated).not.toBeNull();
     });
