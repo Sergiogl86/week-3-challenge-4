e19a405d3508d6872b6972709241d781838b0e8a
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-14 23:15:29 +0200

    Se ha conseguido cargar desde el servidor

--- public/app/js/index.js
+++ public/app/js/index.js
@@ -17,6 +17,27 @@ const film = [
 console.log(film[0].name);
 
 const mainComponent = new Page("body", "container", "div", film);
+
+/*const busquedaPelicula = async () => {
+  try {
+    debugger;
+    const url = "http://localhost:3001/series";
+    const response = await fetch(url);
+    console.log(response);
+    if (response.ok) {
+      const dataResponse = await response.json();
+      console.log(dataResponse);
+      console.log(dataResponse[0]);
+    } else {
+      throw new error("Error provocado :(");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+busquedaPelicula();
+
 //const film1 = new Film("series-list", "serie", "li", film);
 
-//Page("body", "container", "div", film);
+//Page("body", "container", "div", film);*/
